01108f4c1023edf6f3ba76bb0dc5bde749a5705e
sfyn <nkeman200@gmail.com>
2026-09-04 10:13:06 +0700

    chore: inisialisasi dokumen openapi

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMyMDA0NA/spectral.js?branch_slug=main&token=19d53e6a-37b6-4704-a8cf-00c7294e6d9f";
+export default { extends: ruleset };
